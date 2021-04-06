cf30e1a98b8b6a69ca21019a20f75eeab2b62571
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-04-05 21:32:58 -0400

    Add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import CreateArea from "./CreateArea";
-
 import Note from "./Note";
 import Footer from "./Footer";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(text, setText) {
+    setNotes((prevValue) => {
+      return [...prevValue, text];
+    });
+    setText("");
+    event.preventDefault();
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea setNotes={setNotes} onAdd={addNote} />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} />
+      ))}
+      {console.log("notes ", notes)}
       <Note key={1} title="Note title" content="Note Content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [text, setText] = useState({});
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setText((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a Note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={text.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a Note..."
+          onChange={handleChange}
+          value={text.content}
+          rows="3"
+        />
+        <button
+          onClick={() => {
+            props.onAdd(text, setText);
+            setText({});
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
