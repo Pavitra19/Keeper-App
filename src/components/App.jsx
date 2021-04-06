dbb12727748d83ad1f325f4d25c0e6f1ca735a1a
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-04-05 21:01:29 -0400

    Create area component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./Header";
+import CreateArea from "./CreateArea";
+
 import Note from "./Note";
 import Footer from "./Footer";
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       <Note key={1} title="Note title" content="Note Content" />
       <Footer />
     </div>
